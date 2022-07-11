1a4e94edfdb25fc380d1597606944f0caca48bf4
nicoerlando <nicolas.erlando@gmail.com>
2022-07-11 19:31:21 +0800

    created landing page and result page (header only)

---
+++ src/component/pages/SearchResult/SearchResultPage.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "../../header/header";
+import SearchResult from "..";
+// import "./homepage.css"
+// import SearchBar from "../../SearchBar";
+// import { Link } from 'react-router-dom';
+// import { ItineraryData, DayData } from './ItineraryData'
+
+export default function SearchResultPage() {
+  return (
+    <div>
+      <Header />
+      <SearchResult />
+    </div>
+  );
+}
