8542201d78a4bcd076343faa02e36bdac2e1449a
gycc7253 <457776447@qq.com>
2022-07-12 15:04:38 +0800

    added routing from landing page to result page

--- src/component/pages/SearchResult/SearchBar.jsx
+++ src/component/pages/SearchResult/SearchBar.jsx
@@ -1,15 +1,27 @@
 import React, { useState } from "react";
+import {Link, Routes, Route, useNavigate} from 'react-router-dom';
 
-const SearchBar = () => (
-  <form action="/" method="get">
+
+
+
+const SearchBar = () => {
+  const navigate = useNavigate();
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    // 👇️ redirect to /contacts
+    navigate('/result');
+  };
+  return (<form action="https://qv5krif365.execute-api.ap-southeast-1.amazonaws.com/dev/search-query-tagger-lambda" method="post" onSubmit={handleSubmit}>
     <input
       type="text"
-      id="header-search"
+      id="question"
       placeholder="search documentations here..."
       name="s"
     />
     <button type="submit">Search</button>
   </form>
-);
+  )
+};
 
 export default SearchBar;
