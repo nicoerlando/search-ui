5a0c1d604110412630cb6d567abcabf83d164466
nicoerlando <nicolas.erlando@gmail.com>
2022-07-12 23:13:26 +0800

    fix searchResult(s)

--- src/component/pages/SearchResult/SearchBar.jsx
+++ src/component/pages/SearchResult/SearchBar.jsx
@@ -1,67 +1,73 @@
 import React, { useState } from "react";
-import {Link, Routes, Route, useNavigate} from 'react-router-dom';
-
-
-
+import { Link, Routes, Route, useNavigate } from "react-router-dom";
 
 class SearchBar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: '', data: []};
+    this.state = { value: "", data: [] };
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
 
-    this.url_test = 'https://reqres.in/api/posts'
-    this.url_lambda = 'https://qv5krif365.execute-api.ap-southeast-1.amazonaws.com/dev/search-query-tagger-lambda'
+    this.url_test = "https://reqres.in/api/posts";
+    this.url_lambda =
+      "https://qv5krif365.execute-api.ap-southeast-1.amazonaws.com/dev/search-query-tagger-lambda";
     this.navigate = props.navigate;
   }
 
-  handleChange (event) {
-    this.setState({value: event.target.value});
+  handleChange(event) {
+    this.setState({ value: event.target.value });
   }
 
-  handleSubmit (event) {
+  handleSubmit(event) {
     event.preventDefault();
 
-    console.log(this.state.value)
+    console.log(this.state.value);
     // 👇️ redirect to /contacts
     fetch(this.url_lambda, {
-      method: 'POST',
-      body: JSON.stringify({ 'question': this.state.value})
-    }).then(response => {
-      console.log(response.status)
-      if(response.ok){
-        const data = response.json()
-        // this.setState({data: data})
-        return data
-        // return response.json()
-      }else{
-        alert("something is wrong")
-      }
-    }).then(newResults => {
-      console.log(newResults)
-      this.props.setState({data: newResults})
+      method: "POST",
+      body: JSON.stringify({ question: this.state.value }),
     })
-    this.navigate('/result');
-  };
+      .then((response) => {
+        console.log(response.status);
+        if (response.ok) {
+          const data = response.json();
+          // this.setState({data: data})
+          return data;
+          // return response.json()
+        } else {
+          alert("something is wrong");
+        }
+      })
+      .then((newResults) => {
+        console.log(newResults);
+        this.props.setState({ data: newResults });
+      });
+    this.navigate("/result");
+  }
 
   render() {
-    return (<form onSubmit={() => {this.handleSubmit(); this.props.setResult();}}>
-      <input
-        type="text"
-        id="question"
-        placeholder="search documentations here..."
-        name="s"
-        value={this.state.value} 
-        onChange={this.handleChange}
-      />
-      <button type="submit">Search</button>
-    </form>
-    )
+    return (
+      <form
+        onSubmit={() => {
+          this.handleSubmit();
+          this.props.setResults();
+        }}
+      >
+        <input
+          type="text"
+          id="question"
+          placeholder="search documentations here..."
+          name="s"
+          value={this.state.value}
+          onChange={this.handleChange}
+        />
+        <button type="submit">Search</button>
+      </form>
+    );
   }
-};
+}
 
-export default function(props) {
+export default function (props) {
   const navigate = useNavigate();
-  return <SearchBar {...props} navigate={navigate}></SearchBar>
-};
+  return <SearchBar {...props} navigate={navigate}></SearchBar>;
+}
